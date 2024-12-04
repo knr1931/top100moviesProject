ac336d6c64720032d73c0ae5382ef61c254deb55
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2024-12-04 19:48:59 +0530

    Stored url and movies file path as constant variables

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+MOVIES_FILE_PATH = "movies.txt"
+
+response = requests.get(URL)
 movies_webpage = response.text
 
 soup = BeautifulSoup(movies_webpage, 'html.parser')
 
 movies = soup.find_all(name='h3', class_='title')
 
-movies_file_name = "movies.txt"
-
-with open(movies_file_name, 'w', encoding='utf-8') as file:
+with open(MOVIES_FILE_PATH, 'w', encoding='utf-8') as file:
     for movie in movies[-1::-1]:
         file.write(f"{movie.getText()}\n")
