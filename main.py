59f4f319883dfe36d894d1f608629d3ffe67dbe6
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2024-12-04 19:52:43 +0530

    Changed the splicing

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ soup = BeautifulSoup(movies_webpage, 'html.parser')
 movies = soup.find_all(name='h3', class_='title')
 
 with open(MOVIES_FILE_PATH, 'w', encoding='utf-8') as file:
-    for movie in movies[-1::-1]:
+    for movie in movies[::-1]:
         file.write(f"{movie.getText()}\n")
